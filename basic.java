a0279548a7bcbfd13d2bbfa5e02de2f13a6ff1ce
Shobhit Sharma <120013013+shobhit-1801@users.noreply.github.com>
2022-12-07 22:07:56 +0530

    Update basic.java

--- basic.java
+++ basic.java
@@ -1,5 +1,6 @@
-import java.util.*;  // package containing scanner classs --->
-public class basic {  // wap to accept any number n and print sum of square of all no from 1 to n --->
+import java.util.*;  // package containing scanner classs 
+public class basic {  
+     // wap to accept any number n and print sum of square of all no from 1 to n 
      public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
      System.out.println("Enter number ");
